04ad40bc37820144746153146224012229b1aaa7
Felipe Gallassi <felipegallassi@gmail.com>
2022-01-25 22:52:17 -0300

    requisito 1 OK

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código Aqui
+function getSpeciesByIds(...ids) {
+  const especieEscolhida = species.filter((animal) => ids.some((id) => animal.id === id));
+  return especieEscolhida;
 }
 
 module.exports = getSpeciesByIds;

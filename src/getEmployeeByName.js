e93de6c2e331364f493f4fd76644dbec038912c1
Felipe Gallassi <felipegallassi@gmail.com>
2022-01-29 12:46:56 -0300

    requisito 3 OK

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    const nada = {};
+    return nada;
+  }
+  const verificaColab = employees.filter((colabAtual) =>
+    colabAtual.firstName === employeeName || colabAtual.lastName === employeeName)[0];
+  return verificaColab;
 }
+getEmployeeByName();
 
 module.exports = getEmployeeByName;

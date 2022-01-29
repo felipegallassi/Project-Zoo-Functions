0c0561d0f26d0160edabec2aaa0f3513d167f77d
Felipe Gallassi <felipegallassi@gmail.com>
2022-01-29 14:33:07 -0300

    requisito 4 OK

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => (
+    employee.managers.find((manager) => manager === id)
+  ));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const List = employees.filter((employee) => (
+      employee.managers.find((manager) => manager === managerId)
+    ));
+    return List.map((item) => `${item.firstName} ${item.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };

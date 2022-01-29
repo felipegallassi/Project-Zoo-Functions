044413a6f1e4196c826004be8cfd209282993921
Felipe Gallassi <felipegallassi@gmail.com>
2022-01-29 19:29:29 -0300

    requisito 6 OK

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const count = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) count.child += 1;
+    if (entrant.age >= 18 && entrant.age < 50) count.adult += 1;
+    if (entrant.age >= 50) count.senior += 1;
+  });
+
+  return count;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const tickets = Object.entries(countEntrants(entrants));
+  return tickets.reduce((price, ticket) => price + data.prices[ticket[0]] * ticket[1], 0);
 }
 
 module.exports = { calculateEntry, countEntrants };

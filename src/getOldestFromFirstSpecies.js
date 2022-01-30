c02e97fa72b224c9611932322c539bdaa8dbac81
Felipe Gallassi <felipegallassi@gmail.com>
2022-01-30 17:57:02 -0300

    requisito 9 OK

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const specieId = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const specieResidents = data.species.find((specie) => specie.id === specieId).residents;
+  const oldestAnimal = specieResidents.reduce((acc, animal) => {
+    if (acc.age > animal.age) return acc;
+    return animal;
+  });
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;

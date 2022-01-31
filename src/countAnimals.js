265c26c264a364bacc3450cdfc3899e755c363a9
Felipe Gallassi <felipegallassi@gmail.com>
2022-01-30 23:05:09 -0300

    requisito 5 OK

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const animalsList = {};
+    species.forEach((specie) => {
+      animalsList[specie.name] = specie.residents.length;
+    });
+    return animalsList;
+  }
+  const animalTarget = species.find((specie) => specie.name === animal.specie);
+  if (!animal.sex) {
+    return animalTarget.residents.length;
+  }
+  let especifySex = 0;
+  animalTarget.residents.forEach((sexAnimal) => {
+    if (sexAnimal.sex === animal.sex) {
+      especifySex += 1;
+    }
+  });
+  return especifySex;
 }
 
 module.exports = countAnimals;

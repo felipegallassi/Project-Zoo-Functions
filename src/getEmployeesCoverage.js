ed82d4be1d0726611e13c33427c494fef17f3bc3
Felipe Gallassi <felipegallassi@gmail.com>
2022-02-02 23:10:40 -0300

    requisito 10 OK

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const employ = (newObject) => {
+  const responsible = newObject.responsibleFor;
+  const responsibleMap = responsible
+    .map((element) => data.species.find((acc) => acc.id === element).name);
+  const region = responsible
+    .map((element) => data.species.find((acc) => acc.id === element).location);
+  const newEmploye = {
+    id: newObject.id,
+    fullName: `${newObject.firstName} ${newObject.lastName}`,
+    species: responsibleMap,
+    locations: region,
+  };
+  return newEmploye;
+};
+
+function getEmployeesCoverage(employee) {
+  const employeAll = data.employees.map((element) => employ(element));
+  if (employee === undefined) return employeAll;
+  if (employeAll.some((e) => e.id === employee.id) === false && employee.id !== undefined) {
+    throw new Error('Informações inválidas');
+  }
+  const employeDiff = employeAll.find((element) => {
+    if (element.fullName.indexOf(employee.name) >= 0) return element.fullName;
+    return element.id === employee.id;
+  });
+  return employeDiff;
 }
 
 module.exports = getEmployeesCoverage;
